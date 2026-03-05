3f3bfb755bc1fbb3c53eee5efe5df2bb66907b2d
djstarman80 <djstarman80@github.com>
2026-03-05 17:49:27 -0300

    deploy: update github pages

--- android/app/src/main/kotlin/com/bhu/bhu_control_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.bhu.bhu_control_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/utils/web_download_helper_stub.dart
+++
@@ -1,5 +0,0 @@
-class WebDownloadHelper {
-  static void downloadJson(String jsonString, String fileName) {
-    throw UnimplementedError('Solo disponible en web');
-  }
-}
